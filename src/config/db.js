317975d01cc128f704b75ae3b9982658660fe7f4
KevinC29 <73950697+KevinC29@users.noreply.github.com>
2023-01-01 13:37:19 -0500

    Creacion Proyecto Raiz

--- src/config/db.js
+++ src/config/db.js
@@ -10,20 +10,26 @@ import {createPool} from 'mysql2/promise'
 //     database: 'juegoseriodb'
 //  });
 
-import {
-  DB_DATABASE,
-  DB_HOST,
-  DB_PASSWORD,
-  DB_PORT,
-  DB_USER,
-} from "./config.js";
+// import {
+//   DB_DATABASE,
+//   DB_HOST,
+//   DB_PASSWORD,
+//   DB_PORT,
+//   DB_USER,
+// } from "./config.js";
+
+
+// export const DB_HOST = process.env.DB_HOST || "containers-us-west-99.railway.app";
+// export const DB_USER = process.env.DB_USER || "root";
+// export const DB_PASSWORD = process.env.DB_PASSWORD || "ZS8gmKTtD1BCd8sf2Aaw";
+// export const DB_DATABASE = process.env.DB_DATABASE || "railway";
+// export const DB_PORT = process.env.DB_PORT || 5509;
+
 
 export const pool = createPool({
-  host: DB_HOST,
-  user: DB_USER,
-  password: DB_PASSWORD,
-  port: DB_PORT,
-  database: DB_DATABASE,
-  connectionLimit : 10,
-  queueLimit: 0
+  host:  "containers-us-west-99.railway.app",
+  user: "root",
+  password: "ZS8gmKTtD1BCd8sf2Aaw",
+  port: 5509,
+  database:  "railway",
 });
